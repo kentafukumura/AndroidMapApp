455c3d0b609e2e346e3208cb9468d7b206773b2c
kenta_fukumura <kenta_fukumura@sobal.co.jp>
2020-07-15 09:58:08 +0900

    [DD21][課題アプリ No4]GPSによる現在地地図表示機能実装

--- app/src/main/java/com/websarve/wings/android/androidmapapp/GPSActivity.kt
+++ app/src/main/java/com/websarve/wings/android/androidmapapp/GPSActivity.kt
@@ -1,6 +1,12 @@
 package com.websarve.wings.android.androidmapapp
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +15,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 
 /**
  * GPSアクティビティ.
@@ -30,6 +37,7 @@ class GPSActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_g_p_s)
 
+        // 緯度経度の表示
         showPosition()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         Log.i("GPSActivity AppCompatActivity", "end!!")
@@ -43,12 +51,26 @@ class GPSActivity : AppCompatActivity() {
         val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
         val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
 
-        // サンプル表示
+        // 緯度経度の取得
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationListener =  GPSLocationListener()
+
+        if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED ){
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this@GPSActivity, permissions, 1000)
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0 ,0f, locationListener)
+
         tvLatitude.text = "35.018060"
         tvLongitude.text = "138.933563"
         Log.i("GPSActivity showPosition", "end!!")
     }
 
+    /**
+     * onOptionsItemSelected オプション選択
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         Log.i("GPSActivity onOptionsItemSelected", "start!!")
         if(item.itemId == android.R.id.home){
@@ -59,6 +81,9 @@ class GPSActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    /**
+     * onGPSMapButtonClick マップ表示ボタン押下
+     */
     fun onGPSMapButtonClick(view: View) {
         Log.i("GPSActivity onGPSMapButtonClick", "start!!")
         val uriStr = "geo:${_latitude}, ${_longitude}"
@@ -69,5 +94,42 @@ class GPSActivity : AppCompatActivity() {
         Log.i("GPSActivity onGPSMapButtonClick", "startActivity!!")
         startActivity(intent)
         Log.i("GPSActivity onGPSMapButtonClick", "end!!")
+    }
+
+    /**
+     * GPSLocationListener 緯度経度の取得
+     */
+    private inner class GPSLocationListener : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            Log.i("GPSActivity onLocationChanged", "start!!")
+            val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
+            val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
+            tvLatitude.text = location.latitude.toString()
+            tvLongitude.text = location.longitude.toString()
+            _latitude =  location.latitude
+            _longitude = location.longitude
+            Log.i("GPSActivity onLocationChanged", "end!!")
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle){}
+        override fun onProviderEnabled(provider: String){}
+        override fun onProviderDisabled(provider: String){}
+    }
+
+    /**
+     * onRequestPermissionsResult オーバーライド
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray){
+        Log.i("GPSActivity onRequestPermissionsResult", "start!!")
+        if(requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val locationListener = GPSLocationListener()
+            if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+                return
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            Log.i("GPSActivity onRequestPermissionsResult", "end!!")
+        }
     }
 }
